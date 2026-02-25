566be360767f651f1255b781d4837c573d0d776e
ganiular <ganiular@gmail.com>
2026-02-25 10:20:47 +0100

    refactor DBModel to inherit from AppBaseModel for improved serialization

--- app/utils/models/db_model.py
+++ app/utils/models/db_model.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Optional, cast
 
 from bson import ObjectId
 from pydantic import BaseModel, ConfigDict, Field
@@ -7,22 +7,30 @@ from pydantic import BaseModel, ConfigDict, Field
 from app.utils.models.py_object_id import PyObjectId
 
 
-class DBModel(BaseModel):
+class AppBaseModel(BaseModel):
     """
-    Mixin class to add common configuration for database models.
+    Base Model class serialization safe
     """
-
-    id: PyObjectId = Field(
-        default_factory=ObjectId, alias="_id", serialization_alias="id"
-    )
-    time_created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    time_updated: Optional[datetime] = None
 
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
         validate_by_name=True,
         json_encoders={ObjectId: str},
     )
+
+
+class DBModel(AppBaseModel):
+    """
+    Mixin class to add common configuration for database models.
+    """
+
+    id: PyObjectId = Field(
+        default_factory=lambda: cast(PyObjectId, ObjectId()),
+        alias="_id",
+        serialization_alias="id",
+    )
+    time_created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    time_updated: Optional[datetime] = None
 
     def mongo_dump(self):
         data = self.model_dump()
