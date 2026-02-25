3f549de09aa39aebe88cd9c3fa54d08f5c64ddf5
ganiular <ganiular@gmail.com>
2026-02-25 09:16:18 +0100

    set python type checking mode to standard

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.api import api_router
 from app import database
+from app.api.comments.service import CommentService
+from app.api.teams.service import TeamService
+from app.api.users.service import UserService
 from app.exceptions import register_exceptions
 
 
 @asynccontextmanager
 async def db_lifespan(app: FastAPI):
     # Startup
-    await database.open_connection(app)
+    db = await database.open_connection(app)
+
+    # Create database indexes
+    await UserService.create_indexes(db)
+    await TeamService.create_indexes(db)
+    await CommentService.create_indexes(db)
+
     yield
+
     # Shutdown
     await database.close_connection(app)
 
